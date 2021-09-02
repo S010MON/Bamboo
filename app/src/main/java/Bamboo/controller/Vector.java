404dc1330db766c43f5a07567a72aa8f79cc9f89
Alex <alex@ingohartmann.de>
2021-09-02 21:40:24 +0200

    vector class

--- app/src/main/java/Bamboo/controller/Vector.java
+++ app/src/main/java/Bamboo/controller/Vector.java
@@ -2,5 +2,14 @@ package Bamboo.controller;
 
 public class Vector
 {
+    public int x,y;
 
+    public Vector(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public boolean equals(Vector other) {
+        return other.x == this.x && other.y == this.y;
+    }
 }
