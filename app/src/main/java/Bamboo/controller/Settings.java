68d026a370b070d27df8cad441708bc5c1be6659
S010MON <leon@bdebnath.com>
2021-09-06 17:31:31 +0200

    Adds player names, types and boardSize to settings

--- app/src/main/java/Bamboo/controller/Settings.java
+++ app/src/main/java/Bamboo/controller/Settings.java
@@ -2,5 +2,9 @@ package Bamboo.controller;
 
 public class Settings
 {
-
+    public String player1Name;
+    public String player1Type;
+    public String player2Name;
+    public String player2Type;
+    public int boardSize;
 }
