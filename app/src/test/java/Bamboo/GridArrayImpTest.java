b2288afc50300db0ad92aef6d29b5b84acbbd48f
S010MON <leon@bdebnath.com>
2021-09-09 10:28:28 +0200

    Tests getAllNeighbours

--- app/src/test/java/Bamboo/GridArrayImpTest.java
+++ app/src/test/java/Bamboo/GridArrayImpTest.java
@@ -2,14 +2,20 @@ package Bamboo;
 
 import Bamboo.controller.CubeVector;
 import Bamboo.model.Colour;
+import Bamboo.model.Grid;
 import Bamboo.model.GridArrayImp;
 
+import Bamboo.model.Tile;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GridArrayImpTest
 {
-    @Test void test_grid_with_radius_2()
+    @Test void testGridWithRadius2()
     {
         int radius = 2;
         int diameter = (2 * radius) + 1;
@@ -29,5 +35,81 @@ class GridArrayImpTest
             }
         }
     }
+
+    /*
+     * Testing Schema - Get all neighbours
+     * Centre       [0,0,0] -> [0,1,-1],[0,-1,1],[1,0,-1],[1,-1,0],[-1,0,1],[-1,1,0]
+     * Off-Centre   [-1,1,0] -> [0,0,0],[0,1,-1],[-1,2,-1],[-1,0,1],[-2,1,1],[-2,2,0]
+     * Edge         [-2,2,0] -> [-1,2,-1],[-1,1,0],[-2,1,1]
+     */
+
+    @Test void testGetAllNeighbours_centre()
+    {
+        ArrayList<Tile> exp = new ArrayList<>();
+        exp.add(new Tile(new CubeVector( 0, 1,-1)));
+        exp.add(new Tile(new CubeVector( 0,-1, 1)));
+        exp.add(new Tile(new CubeVector( 1, 0,-1)));
+        exp.add(new Tile(new CubeVector( 1,-1, 0)));
+        exp.add(new Tile(new CubeVector(-1, 0, 1)));
+        exp.add(new Tile(new CubeVector(-1, 1, 0)));
+
+        Grid grid = new GridArrayImp(3);
+        ArrayList<Tile> act = (ArrayList<Tile>) grid.getAllNeighbours(new CubeVector(0,0,0));
+
+        for(Tile tile: exp)
+        {
+            CubeVector v = tile.getVector();
+            assertTrue(listContainsVector(act, v));
+        }
+    }
+
+    @Test void testGetAllNeighbours_offCentre()
+    {
+        ArrayList<Tile> exp = new ArrayList<>();
+        exp.add(new Tile(new CubeVector( 0, 0, 0)));
+        exp.add(new Tile(new CubeVector( 0, 1,-1)));
+        exp.add(new Tile(new CubeVector(-1, 2,-1)));
+        exp.add(new Tile(new CubeVector(-1, 0, 1)));
+        exp.add(new Tile(new CubeVector(-2, 1, 1)));
+        exp.add(new Tile(new CubeVector(-2, 2, 0)));
+
+        Grid grid = new GridArrayImp(3);
+        ArrayList<Tile> act = (ArrayList<Tile>) grid.getAllNeighbours(new CubeVector(-1,1,0));
+
+        for(Tile tile: exp)
+        {
+            CubeVector v = tile.getVector();
+            assertTrue(listContainsVector(act, v));
+        }
+    }
+
+    @Test void testGetAllNeighbours_edge()
+    {
+        ArrayList<Tile> exp = new ArrayList<>();
+        exp.add(new Tile(new CubeVector(-1, 2,-1)));
+        exp.add(new Tile(new CubeVector(-1, 1, 0)));
+        exp.add(new Tile(new CubeVector(-1, 1, 1)));
+
+        Grid grid = new GridArrayImp(3);
+        ArrayList<Tile> act = (ArrayList<Tile>) grid.getAllNeighbours(new CubeVector(-1,1,0));
+
+        for(Tile tile: exp)
+        {
+            CubeVector v = tile.getVector();
+            assertTrue(listContainsVector(act, v));
+        }
+    }
+
+    private boolean listContainsVector(List<Tile> list, CubeVector vector)
+    {
+        for(Tile t: list)
+        {
+            if(t.getVector().equals(vector))
+                return true;
+        }
+        return false;
+    }
+
+
 
 }
