921cc2a22862e468d41c30c8df362364c91166a0
shirleydekoster <72269297+shirleydekoster@users.noreply.github.com>
2021-09-15 12:41:59 +0200

    test

--- app/src/main/java/Bamboo/view/GamePanel.java
+++ app/src/main/java/Bamboo/view/GamePanel.java
@@ -2,25 +2,40 @@ package Bamboo.view;
 
 import Bamboo.model.Game;
 
-import javax.swing.JPanel;
+import javax.swing.*;
 import java.awt.Dimension;
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GamePanel extends JPanel
+
 {
     private Game game;
     private Canvas canvas;
+    private JPanel panel1;
+    private JButton pauseButton;
 
     public GamePanel(Dimension screenSize, Game game)
     {
-        this.game = game;
+       // test comment
+        //this.game = game;
 
-        setSize(screenSize);
-        setLayout(new BorderLayout());
+       //setSize(screenSize);
+       //setLayout(new BorderLayout());
 
-        canvas = new Canvas(screenSize, game);
-        add(canvas, BorderLayout.CENTER);
+       //canvas = new Canvas(screenSize, game);
+       //add(canvas, BorderLayout.CENTER);
 
-        setVisible(true);
+       //setVisible(true);
+       //pauseButton.addActionListener(new ActionListener() {
+       //    @Override
+       //    public void actionPerformed(ActionEvent e) {
+
+       //    }
+       //});
     }
+
+
+
 }
