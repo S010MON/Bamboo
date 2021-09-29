a4e9ee3695076ad51430b6c3fed86393936eb3dd
shirleydekoster <72269297+shirleydekoster@users.noreply.github.com>
2021-09-29 17:22:43 +0200

    game panel labels

--- app/src/main/java/Bamboo/view/game/InfoPanel.java
+++ app/src/main/java/Bamboo/view/game/InfoPanel.java
@@ -1,40 +1,47 @@
 package Bamboo.view.game;
 
 import Bamboo.model.Game;
+import Bamboo.view.startup.Label;
 
 import javax.swing.*;
 import java.awt.*;
 
+
 public class InfoPanel extends JPanel {
     private Color background = new Color(158, 208, 239) ;
-    private JLabel numberOfGroupsLabel;
     private JLabel numberOfGroups;
     private  JLabel maxSizeOfGroupsLabel;
     private JLabel maxSizeOfGroups;
     private  JLabel player;
     private JLabel currentPlayer;
+    private JLabel numberOfGroupsLabel;
 
 
 
     public InfoPanel(String firstPlayer)
     {
+
+
         setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
         setBackground(background);
 
 
-        numberOfGroupsLabel = new JLabel("NumberOfGroups:");
+        numberOfGroupsLabel = new  JLabel("Number of groups:");
+        numberOfGroupsLabel.setFont(new Font("Monospaced", Font.PLAIN, 18));
         add(numberOfGroupsLabel);
 
         numberOfGroups = new JLabel("0");
         add(numberOfGroups);
 
-        maxSizeOfGroupsLabel = new JLabel("MaxSize:");
+        maxSizeOfGroupsLabel = new JLabel("Maximum size group:");
+        maxSizeOfGroupsLabel.setFont(new Font("Monospaced", Font.PLAIN, 18));
         add(maxSizeOfGroupsLabel);
 
         maxSizeOfGroups = new JLabel("0");
         add(maxSizeOfGroups);
 
         player = new JLabel("Player:");
+        player.setFont(new Font("Monospaced", Font.PLAIN, 18));
         add(player);
 
         currentPlayer = new JLabel(firstPlayer);
