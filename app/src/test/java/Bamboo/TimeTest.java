3370d55935b398a74a971bd1840dffd89c99722f
shirleydekoster <72269297+shirleydekoster@users.noreply.github.com>
2021-10-06 12:34:56 +0200

    timing test

---
+++ app/src/test/java/Bamboo/TimeTest.java
@@ -0,0 +1,23 @@
+package Bamboo;
+
+import org.junit.jupiter.api.Test;
+
+
+import java.sql.Time;
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+
+public class TimeTest {
+    @Test void testTime() {
+        long startTime = System.currentTimeMillis();
+        long finishTime = System.currentTimeMillis();
+        long totalTime = finishTime - startTime;
+    }
+
+
+
+}
