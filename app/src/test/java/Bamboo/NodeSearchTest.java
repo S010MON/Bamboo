83e356be00e50c777521f763f21d7b2f5b97119f
Alex <alex@ingohartmann.de>
2021-10-28 11:52:10 +0200

    fixed testing

--- app/src/test/java/Bamboo/NodeSearchTest.java
+++ app/src/test/java/Bamboo/NodeSearchTest.java
@@ -15,6 +15,7 @@ public class NodeSearchTest {
         NodeMM start = new NodeMM(new GridGraphImp(2));
         MiniMax agent = new MiniMax(Color.RED);
         agent.setGame(new ArrayList<>(start.getGrid().getAllVectors()));
+        agent.minimax(start,2,true);
         assertEquals(start.getChildren().get(0).getChildren().size(), new GridGraphImp(2).getAllTiles().size() - 1);
     }
 }
