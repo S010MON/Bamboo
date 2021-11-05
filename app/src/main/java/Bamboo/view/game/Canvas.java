7079ec9d0310e07cd889cc1aab1f8f058eb59519
Benjum9 <72268439+Benjum9@users.noreply.github.com>
2021-11-05 12:20:31 +0100

    hint modified

--- app/src/main/java/Bamboo/view/game/Canvas.java
+++ app/src/main/java/Bamboo/view/game/Canvas.java
@@ -10,11 +10,7 @@ import Bamboo.view.resources.ResourceLoader;
 
 import javax.swing.JPanel;
 import javax.swing.Timer;
-import java.awt.BasicStroke;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.util.HashMap;
 
@@ -50,6 +46,8 @@ public class Canvas extends JPanel
         images.put(Color.RED, ResourceLoader.getImage("red_circle.png"));
         images.put(Color.BLUE, ResourceLoader.getImage("blue_circle.png"));
         images.put(Color.WHITE,ResourceLoader.getImage("white_circle.png"));
+        images.put(new Color(44, 154, 21),ResourceLoader.getImage("green_circle.png")) ;
+        images.put(Color.GRAY,ResourceLoader.getImage("gray_circle.png")) ;
         
         if(game.isAgentVsAgent())
         {
@@ -107,7 +105,11 @@ public class Canvas extends JPanel
         int x = centreX + (v.getQ() * circle_radius / 2);
         int y = centreY + (v.getR() * circle_radius / 2);
 
-        g2d.drawImage(images.get(color),x,y,circle_radius,circle_radius,null);
+        if(color==Color.GRAY)
+            g2d.drawImage(images.get(color),x-8,y-8,circle_radius+17,circle_radius+17,null);
+        else
+            g2d.drawImage(images.get(color),x,y,circle_radius,circle_radius,null);
+
 
         //g2d.setStroke(tile.getCircle_thickness());
         //g2d.setColor(color);
@@ -135,14 +137,24 @@ public class Canvas extends JPanel
         v = VectorConverter.doubleAndOffsetOddRows(v);
         int x = centreX + (v.getQ() * circle_radius/2) ;
         int y = centreY + (v.getR() * circle_radius/2) ;
+        Color color = new Color(44, 154, 21) ;
+        Color color2 = Color.GRAY ;
 
         if(game.getGrid().isLegalMove(tile.getVector(), game.getCurrentPlayer().getColor()))
         {
-            g2d.setStroke(tile.getCircle_thickness());
-            g2d.setColor(new Color(44, 154, 21));
-            g2d.fillOval(x+14,y+14,circle_radius-30,circle_radius-30);
-            g2d.setColor(tile.getOutline());
-            g2d.drawOval(x, y, circle_radius, circle_radius);
+            //g2d.setStroke(tile.getCircle_thickness());
+            //g2d.setColor(color = new Color(44, 154, 21));
+            g2d.drawImage(images.get(color),x+3,y+3,circle_radius-6,circle_radius-6,null);
+            //g2d.fillOval(x+14,y+14,circle_radius-30,circle_radius-30);
+           // g2d.setColor(tile.getOutline());
+            //g2d.drawOval(x, y, circle_radius, circle_radius);
+        }
+        else{
+          //  if(tile.getColour()!=Color.BLUE && tile.getColour()!=Color.RED) {
+            if(tile.getColour()==Color.WHITE){
+               // g2d.setColor(color = Color.GRAY);
+                g2d.drawImage(images.get(color2), x - 8, y - 8, circle_radius + 17, circle_radius + 17, null);
+            }
         }
     }
 
