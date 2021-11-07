b579e676bf81ed38804be7de2407a40594c0d992
S010MON <leon@bdebnath.com>
2021-11-07 20:54:03 +0100

    Adds network manager skeleton class

---
+++ app/src/main/java/Bamboo/controller/nNet/NetworkManager.java
@@ -0,0 +1,16 @@
+package Bamboo.controller.nNet;
+
+import deepnetts.net.FeedForwardNetwork;
+
+public class NetworkManager
+{
+    public void save(FeedForwardNetwork n, String filePath)
+    {
+
+    }
+
+    public FeedForwardNetwork load(String filePath)
+    {
+        return null;
+    }
+}
