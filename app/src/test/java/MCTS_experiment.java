501dfe23dee9cbbd8655f7a4e5cda6535cc06db4
Benjum9 <72268439+Benjum9@users.noreply.github.com>
2021-11-11 13:20:56 +0100

    new branch test mcts

--- app/src/test/java/MCTS_experiment.java
+++ app/src/test/java/MCTS_experiment.java
@@ -7,6 +7,7 @@ import Bamboo.model.GameWithoutGUI;
 
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MCTS_experiment
 {
@@ -14,7 +15,10 @@ public class MCTS_experiment
         double startC = 0.05;
         double stepSize = 0.05;
         double maxC = 1;
-        int replications = 1;
+        int replications = 100;
+        int iterrationStep = 50;
+        int[][] wins = new int[20][20];
+
         int boardSize = 5;
         ArrayList<Double> winRates = new ArrayList<>();
 
@@ -23,27 +27,31 @@ public class MCTS_experiment
         // iterations here >= 100
         // C in (0,1)
 
-        for(double C = startC; C <= maxC; C+=stepSize){
-            int[] wins = new int[replications];
+        for (double C = startC; C < maxC; C += stepSize) {
+            //int[][] wins = new int[replications];
             UCB.C = C;
             System.out.println(C);
-            for(int i = 0; i < replications; i++){
-                GameWithoutGUI.MCTSiterations = 100;//just an idea
-                Agent agent1 = AgentFactory.makeAgent(AgentType.MCTS, Color.RED);
-                Agent agent2 = AgentFactory.makeAgent(AgentType.RANDOM, Color.BLUE);
-                Settings settings = new Settings(agent1, agent2, boardSize);
-                GameWithoutGUI game = new GameWithoutGUI(settings);
-                Agent winner = game.turnLogic();//make this return winning agent in the end
-                if(winner == agent1)
-                    wins[i] = 1;
-                else
-                    wins[i] = 0;
+            for (int j = 1; j < 20; j++) {
+                for (int i = 0; i < replications; i++) {
+
+                    GameWithoutGUI.MCTSiterations = j * iterrationStep;//just an idea
+                    Agent agent1 = AgentFactory.makeAgent(AgentType.MCTS, Color.RED);
+                    Agent agent2 = AgentFactory.makeAgent(AgentType.RANDOM, Color.BLUE);
+                    Settings settings = new Settings(agent1, agent2, boardSize);
+                    GameWithoutGUI game = new GameWithoutGUI(settings);
+                    Agent winner = game.turnLogic();//make this return winning agent in the end
+                    if (winner == agent1)
+                        wins[i][j] = wins[i][j] + 1;
+                }
             }
-            winRates.add(mean(wins));
+
+            // winRates.add(mean(wins));
         }
-        for(double p : winRates)
-            System.out.println(p);
+
+       // for(double p : winRates)
+           // System.out.println(p);
         //todo complete setup and write to file
+        System.out.println(Arrays.deepToString(wins));
     }
 
     private static double mean(int[] wins) {
