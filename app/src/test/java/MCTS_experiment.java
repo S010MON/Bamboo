e99780af44434ac619b7de804628dffd6a3f5140
Benjum9 <72268439+Benjum9@users.noreply.github.com>
2021-11-11 14:15:37 +0100

    test implemented with right values

--- app/src/test/java/MCTS_experiment.java
+++ app/src/test/java/MCTS_experiment.java
@@ -12,11 +12,15 @@ import java.util.Arrays;
 public class MCTS_experiment
 {
     public static void main(String[] args) {
+        // I think these are the good values you just have to run it, the n*n matrix will contains average of the wins
+        // Maybe check the code before to see if there is no error left :)
         double startC = 0.05;
         double stepSize = 0.05;
         double maxC = 1;
-        int replications = 100;
+        int replications = 1;
         int iterrationStep = 50;
+        int num_game = 0 ;
+
         int[][] wins = new int[20][20];
 
         int boardSize = 5;
@@ -27,22 +31,28 @@ public class MCTS_experiment
         // iterations here >= 100
         // C in (0,1)
 
-        for (double C = startC; C < maxC; C += stepSize) {
+        for (int k = 1; k <= 20; k++ ) {                       //Loop to change C value
             //int[][] wins = new int[replications];
-            UCB.C = C;
-            System.out.println(C);
-            for (int j = 1; j < 20; j++) {
-                for (int i = 0; i < replications; i++) {
 
-                    GameWithoutGUI.MCTSiterations = j * iterrationStep;//just an idea
+            UCB.C = startC+stepSize;
+            stepSize=stepSize+stepSize ;
+            //System.out.println(C);
+            for (int j = 1; j <= 20; j++) {                  //inner loop to change num itterartion
+                for (int i = 0; i < replications; i++) {    //inner-inner loop to play n games (n=replications)
+
+                    GameWithoutGUI.MCTSiterations = j +iterrationStep;
+                    iterrationStep=iterrationStep+iterrationStep;
+
                     Agent agent1 = AgentFactory.makeAgent(AgentType.MCTS, Color.RED);
                     Agent agent2 = AgentFactory.makeAgent(AgentType.RANDOM, Color.BLUE);
                     Settings settings = new Settings(agent1, agent2, boardSize);
                     GameWithoutGUI game = new GameWithoutGUI(settings);
                     Agent winner = game.turnLogic();//make this return winning agent in the end
                     if (winner == agent1)
-                        wins[i][j] = wins[i][j] + 1;
+                        wins[k][j] = wins[k][j] + 1;
                 }
+                num_game++ ;
+                System.out.println(num_game);
             }
 
             // winRates.add(mean(wins));
@@ -51,6 +61,7 @@ public class MCTS_experiment
        // for(double p : winRates)
            // System.out.println(p);
         //todo complete setup and write to file
+        mean(wins);
         System.out.println(Arrays.deepToString(wins));
     }
 
