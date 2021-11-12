a4916d8ddb7a2b48cb013645245df69277984587
AlexHartmann00 <alex@ingohartmann.de>
2021-11-12 13:25:05 +0100

    Set up for experiments. Changed a few things to make it more adjustable

--- app/src/test/java/MCTS_experiment.java
+++ app/src/test/java/MCTS_experiment.java
@@ -1,8 +1,5 @@
-import Bamboo.controller.Agent;
-import Bamboo.controller.AgentFactory;
-import Bamboo.controller.AgentType;
+import Bamboo.controller.*;
 import Bamboo.controller.MCTS.UCB;
-import Bamboo.controller.Settings;
 import Bamboo.model.GameWithoutGUI;
 
 import java.awt.*;
@@ -14,17 +11,21 @@ public class MCTS_experiment
     public static void main(String[] args) {
         // I think these are the good values you just have to run it, the n*n matrix will contains average of the wins
         // Maybe check the code before to see if there is no error left :)
-        double startC = 0.05;
+        double startC = 0.2;
         double stepSize = 0.05;
-        double maxC = 1;
+        double maxC = 0.8;
         int replications = 100;
-        int iterationStep = 50;
+        int startIterations = 1;
+        int iterationStep = 250;
+        int maxIterations = 1001;
         int num_game = 0 ;
 
-        double[][] wins = new double[20][20];
+
+        int rows = 1 + (int)Math.round((maxC-startC)/stepSize), cols = (int) (1 + Math.ceil((double)(maxIterations - startIterations)/iterationStep));
+        double[][] wins = new double[rows][cols];
 
         int boardSize = 5;
-        ArrayList<Double> winRates = new ArrayList<>();
+        int total = rows*cols*replications;
 
         //I think:
         // MCTS iterations <= 1000
@@ -33,50 +34,60 @@ public class MCTS_experiment
 
         //The loops are bases on the indexes of the array and not on the values of C and iteration
         // steps are increased manually in the loop
+        int index_c = 0;
+        int index_i = 0;
 
-        for (int k = 1; k <= 20; k++ ) {                       //Loop to change C value
+        for (double C = startC; C <= maxC; C+=stepSize) {                       //Loop to change C value
             //int[][] wins = new int[replications];
 
-            UCB.C = startC+stepSize;
-            stepSize=stepSize+stepSize ;
+            UCB.C = C;
             //System.out.println(C);
-            for (int j = 1; j <= 20; j++) {                  //inner loop to change num itterartion
+            for (int iter = startIterations; iter <= maxIterations; iter += iterationStep) {//inner loop to change num itterartion
+                int[] records = new int[replications];
                 for (int i = 0; i < replications; i++) {    //inner-inner loop to play n games (n=replications)
 
-                    GameWithoutGUI.MCTSiterations = j +iterationStep;
-                    iterationStep=iterationStep+iterationStep;
+                    GameWithoutGUI.MCTSiterations = iter;
 
                     Agent agent1 = AgentFactory.makeAgent(AgentType.MCTS, Color.RED);
                     Agent agent2 = AgentFactory.makeAgent(AgentType.RANDOM, Color.BLUE);
                     Settings settings = new Settings(agent1, agent2, boardSize);
                     GameWithoutGUI game = new GameWithoutGUI(settings);
-                    Agent winner = game.turnLogic();//make this return winning agent in the end
+                    Agent winner = game.turnLogic();//make this return winning agent in the end;  ---  I think this is weirdly named?
                     if (winner == agent1)
-                        wins[k-1][j-1] = wins[k-1][j-1] + 1;
+                        records[i] = 1;
+                    else
+                        records[i] = 0;
                     num_game++ ;  //just to see at which number of game you are while running
-                    System.out.println(num_game);
+                    System.out.println("Game " + num_game + "out of " + total+ " processed, " + 100*(float)num_game/total + "%.");
                 }
-
+                //System.out.println("C:"+C+", I:"+iter+", array["+index_c+"]["+index_i+"] = " + mean(records));
+                wins[index_c][index_i] = mean(records);
+                index_i++;
             }
+            index_c ++;
+            index_i = 0;
 
             // winRates.add(mean(wins));
         }
 
-       // for(double p : winRates)
-           // System.out.println(p);
-        //todo complete setup and write to file
-        mean(wins);
+        String writeString = "";
+
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                writeString += wins[i][j] + ",";
+            }
+             writeString += "\n";
+        }
+        Logger.logCSV("MCTS_data",writeString);
+
         System.out.println(Arrays.deepToString(wins));
     }
 
-    private static void mean(double[][] wins)
-    {
-        for (int i = 0; i<wins.length; i++)
-        {
-            for(int j = 0; j< wins[i].length; j++)
-            {
-                wins[i][j] = wins[i][j] / 100;
-            }
+    private static double mean(int[] x){
+        int sum = 0;
+        for(int i : x){
+            sum += i;
         }
-    }//return mean of array
+        return sum/(double)x.length;
+    }
 }
