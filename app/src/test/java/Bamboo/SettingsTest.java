11722c1ca8baee935a896fa1e28494980a60851b
S010MON <leon@bdebnath.com>
2021-11-12 14:46:38 +0100

    Adds test to reproduce error

---
+++ app/src/test/java/Bamboo/SettingsTest.java
@@ -0,0 +1,21 @@
+package Bamboo;
+
+import Bamboo.controller.Agent;
+import Bamboo.controller.Settings;
+import Bamboo.controller.random.Random;
+
+import java.awt.Color;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class SettingsTest
+{
+    @Test void currentPlayerNotNull()
+    {
+        Agent agent1 = new Random(Color.BLUE);
+        Agent agent2 = new Random(Color.RED);
+        Settings settings = new Settings(agent1, agent2, 5);
+        assertNotNull(settings.getCurrentPlayer());
+    }
+}
