780ecd7e3550e2eedc8e528303a33d157f983b2c
S010MON <leon@bdebnath.com>
2022-01-03 13:09:32 +0000

    Adds empty OuterWeighted Heuristic class

---
+++ app/src/main/java/Bamboo/controller/heuristics/OuterWeighted.java
@@ -0,0 +1,13 @@
+package Bamboo.controller.heuristics;
+
+import Bamboo.controller.Vector;
+import Bamboo.model.Game;
+
+public class OuterWeighted implements Heuristic
+{
+
+    @Override
+    public Vector getNextMove(Game game) {
+        return null;
+    }
+}
