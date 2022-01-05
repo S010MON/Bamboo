71ab3d46a38892ca376a3a169c39c7e122cf63c3
AlexHartmann00 <87905364+AlexHartmann00@users.noreply.github.com>
2022-01-05 10:35:40 +0100

    new training data (40000 MiniMax v Random samples)

--- app/src/main/java/Bamboo/App.java
+++ app/src/main/java/Bamboo/App.java
@@ -1,11 +1,14 @@
 package Bamboo;
 
+import Bamboo.controller.nNet.NetworkTraining;
 import Bamboo.view.MainFrame;
+
+import java.io.IOException;
 
 public class App
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws IOException {
+        NetworkTraining.train();
         MainFrame frame = new MainFrame();
         frame.setVisible(true);
     }
